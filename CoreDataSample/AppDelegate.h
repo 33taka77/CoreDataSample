28ed95a61e85f2d1fcfa37dc90b04befda1d057c
Aizawa Takashi <aizawatakashi@Aizawa-no-MacBook-Pro.local>
2014-04-10 13:59:12 +0900

    Initial Commit

---
+++ CoreDataSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CoreDataSample
+//
+//  Created by Aizawa Takashi on 2014/04/10.
+//  Copyright (c) 2014年 Aizawa Takashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
